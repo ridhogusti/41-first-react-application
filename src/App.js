6a67ad723d4475a52db0f93be63635ac38529a22
Ar Ridho Gusti <ridhogusti0@gmail.com>
2022-12-01 11:35:19 +0700

    fix: add setting camera

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
-import './index.css'
-import Experience from './Experience'
+import { ACESFilmicToneMapping, sRGBEncoding } from "three";
+import "./index.css";
+import Experience from "./Experience";
 
+const cameraSettings = {
+  fov: 45,
+  near: 0.1,
+  far: 200,
+  position: [3, 2, 6],
+};
 function App() {
   return (
     <>
-      <Canvas>
+      <Canvas
+        dpr={[1, 2]}
+        gl={{
+          antialias: true,
+          toneMapping: ACESFilmicToneMapping,
+          outputEncoding: sRGBEncoding,
+        }}
+        camera={cameraSettings}
+      >
         <Experience />
       </Canvas>
     </>
--- src/Experience.js
+++ src/Experience.js
@@ -11,6 +11,11 @@ const Experience = () => {
   const groupRef = useRef();
 
   useFrame((state, delta) => {
+    // const angle = state.clock.elapsedTime;
+    // state.camera.position.x = Math.sin(angle) * 8;
+    // state.camera.position.z = Math.cos(angle) * 8;
+    // state.camera.lookAt(0, 0, 0);
+
     cubeRef.current.rotation.y += delta;
     // groupRef.current.rotation.y += delta;
   });
